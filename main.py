dc7c58ed2d659b0ff5c93f035f194bfbf7d25115
andreiraphel <andreiraphel@gmail.com>
2024-07-12 08:07:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from PIL import Image
 
-im = Image.open('W:/pass/google-1.png')
+# Open the image file
+def main():
+    with Image.open('W:/pass/google-1.png') as img:
+        height, width = img.size
+        pixels = img.load()
 
-print(im.format, im.size, im.mode)
+        # Change these !!!
+        # Hidden message
+        msg = 'secret'
+        hidden_interval = 10
 
-im.show()
+        binary_list = []
+        for x in msg:
+            binary_list.append(stringToBinary(x))
+        
+        # Pixel interval
+        print(len(msg))
+        interval_height = len(msg)
+        interval_width = interval_height + hidden_interval
+
+        print(binary_list)
+        selected_pixels = [(x,y) for x in range(0, height, interval_height) for y in range(0, width, interval_width)]
+
+def stringToBinary(n):
+    return ''.join(format(ord(n), 'b'))
+
+if __name__ == '__main__':
+    main()
